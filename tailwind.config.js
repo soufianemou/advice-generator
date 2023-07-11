e44accf48a12a303aa018fd06f2fc1a3fa720c91
soufianemou <soufianemouahhidi7@gmail.com>
2023-07-11 23:41:50 +0100

    all file has added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","./index.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'manrope': ['Manrope'],
+    },
+    // colors: {
+    //   'Light-Cyan': '#cee3e9',
+    //   'Neon-Green': '	#52ffa8',
+    //   'Grayish-Blue': '	#4e5d73',
+    //   'Dark-Grayish-Blue': '#323a49',
+    //   'Dark-Blue': '#1f2632',
+    // },
+  },
+  plugins: [],
+}
